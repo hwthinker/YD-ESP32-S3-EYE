5e293490616e37a22ee118b390aee937522e76a7
hwtinker <hwthinker@gmail.com>
2026-05-12 07:24:31 +0700

    delete yang gak perlu

--- circuit-python_old_not-recommender/circuitpython-firmware/circuitpython-community-bundle-8.x-mpy-20231129/circuitpython-community-bundle-8.x-mpy-20231129/examples/ad9833_simpletest.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023 JG for Cedar Grove Maker Studios
-#
-# SPDX-License-Identifier: Unlicense
--- circuit-python_old_not-recommender/circuitpython-firmware/circuitpython-community-bundle-8.x-mpy-20231129/circuitpython-community-bundle-8.x-mpy-20231129/examples/hmac_simpletest.py
+++
@@ -1,6 +0,0 @@
-import circuitpython_hmac as hmac
-
-secret = b"secret"
-msg = "message"
-
-key = hmac.new(secret, msg=msg).digest()
